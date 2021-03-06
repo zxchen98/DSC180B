b7d717b90a5443ee2a6a4d5145bc9ed0696d5ef4
Xingyu Jiang <49181693+cherrito@users.noreply.github.com>
2021-03-06 01:27:21 -0800

    Update generatefinaldatf.py

--- src/generatefinaldatf.py
+++ src/generatefinaldatf.py
@@ -10,6 +10,27 @@ def generate_final_dataframe(lastdataf_link, nonzeo_link,zero_link):
   nonzero0m = zxc[zxc['M']!=0]
   zerom = zxc[zxc['M']==0]
   
+  nzerom['M']=np.log(nzerom['M'])
+  zerom['M']=np.log(zerom['M']+1)
+  
+  nzerom['views']=np.log(nzerom['views'])
+  zerom['views']=np.log(zerom['views'])
+  
+  nzerom['sentiment_score']=abs(nzerom['sentiment_score'])
+  zerom['sentiment_score']=abs(zerom['sentiment_score'])
+  
+  
+  nzero_title=nzerom.groupby('title').agg({'revert':'mean', 
+                           'views':'mean', 
+                           'sentiment_score':'mean', 
+                           'M': 'mean'})
+
+  zero_title=zerom.groupby('title').agg({'revert':'mean', 
+                           'views':'mean', 
+                           'sentiment_score':'mean', 
+                           'M': 'mean'})
+
+  
   nonzero0m.to_csv(nonzeo_link,index=False)
   zerom.to_csv(zero_link,index=False)
   
