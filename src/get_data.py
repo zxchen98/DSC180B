ebe02ad796b51ccdb2e8910030c837253325290c
Xingyu Jiang <49181693+cherrito@users.noreply.github.com>
2021-03-06 00:32:06 -0800

    Update get_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -15,59 +15,64 @@ import zipfile
 def find_count(name,name_dict):
     name_dict[name]+=1
     return name_dict[name]-1
-    
+
+def getMtest(url, to_csvname):
+    URL = url 
+    r = requests.get(URL, allow_redirects=True)
+    open(to_csvname, 'wb').write(r.content)
+
 
 def download_xml_file(url, to_csvfilename):
     URL = url 
-    filename = os.path.basename(URL)
-    filehandle, _ = urllib.request.urlretrieve(URL)
-    zip_file_object = bz2.BZ2File(filehandle)
-    data = zip_file_object.read()
-    newfilepath = 'test/output/'+filename[:-4]
-    open(newfilepath, 'wb').write(data)
+    filename = os.path.basename(url)
+    urllib.request.urlretrieve(url,filename)
+    newfilepath = filename[:-4]
+    a1=open(newfilepath,'wb')
+    with bz2.BZ2File(filename, "r") as file:
+        for line in file:
+            a1.write(line)
+    a1.close()
 
   #parse the xml file 
   #convert the raw xml to pd.dataframe
-    infile = open(newfilepath,"rb")
-    contents = infile.read()
-    soup = BeautifulSoup(contents,'html.parser')
-
-    all_revision=soup.find_all('revision')
-
     revision_list=[]
-    for i in all_revision:
-        each_revision={}
-        #find contributor of every revision
-        contributor=i.find('contributor')
-        try:
-            username=contributor.find('username').get_text()
-        except:
-            username=np.nan
-        each_revision['Contributor_Name']=username
+    a=0
+    title_list=[]
+    for event, elem in ET.iterparse(newfilepath,events=('end',)):
+        if elem.tag=='{http://www.mediawiki.org/xml/export-0.10/}title':
+            title_list.append(elem.text)
+        if elem.tag=='{http://www.mediawiki.org/xml/export-0.10/}revision':
+            each_revision={'Title':title_list[-1]}
+            #find contributor of every revision
+            contributor=elem.find('{http://www.mediawiki.org/xml/export-0.10/}contributor')
+            try:
+                username=contributor.find('{http://www.mediawiki.org/xml/export-0.10/}username').text
+            except:
+                username=np.nan
+            each_revision['Contributor_Name']=username
 
-        #find each revision time
-        time=i.find('timestamp').get_text()
-        each_revision['time']=time
+            #find each revision time
+            time=elem.find('{http://www.mediawiki.org/xml/export-0.10/}timestamp').text
+            each_revision['time']=time
 
-        #find each revision comment
-        try:
-            comment=i.find('comment').get_text()
-        except:
-            comment=np.nan
-        each_revision['comment']=comment 
+            #find each revision comment
+            try:
+                comment=elem.find('{http://www.mediawiki.org/xml/export-0.10/}comment').text
+            except:
+                comment=np.nan
+            each_revision['comment']=comment 
 
-        revision_list.append(each_revision)
+            revision_list.append(each_revision)
+
+            if len(revision_list)>=90000:
+                df=pd.DataFrame(revision_list)
+                df.to_csv('raw_dump/raw_dump_b'+str(a)+'.csv')
+                revision_list=[]
+                a+=1
+            elem.clear()
 
     df=pd.DataFrame(revision_list)
-
-    name_count={}
-    all_contributor=df['Contributor_Name'].unique()
-    for i in all_contributor:
-        name_count[i]=0
-
-    count_copy=name_count.copy()
-    df['Revision Time']=df['Contributor_Name'].apply(find_count,args=(count_copy,))
-    
-    firstresult = df.to_csv(to_csvfilename,index=False)
-    return firstresult
+    df.to_csv(to_csvfilename)
+   
+    return df
   
